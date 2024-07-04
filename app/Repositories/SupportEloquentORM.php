1716b5fd22428583e63c9de954594c781d517578
saramendesoliveiralima <sara.m.o.lima@gmail.com>
2024-07-04 10:28:23 -0300

    Aula 26

--- app/Repositories/SupportEloquentORM.php
+++ app/Repositories/SupportEloquentORM.php
@@ -29,7 +29,8 @@ class SupportEloquentORM implements SupportRepositoryInterface
         })
                 ->paginate($totalPerPage, ['*'],'page',$page);
 
-        dd ($result);
+        dd((new PaginationPresenter($result))->items());
+        return new PaginationPresenter($result);
     }
 
 
